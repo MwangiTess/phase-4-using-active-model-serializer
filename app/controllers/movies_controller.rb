40681c5fc5310143085d51100ba93469e43836ed
Teresia <teresiamwangi26@gmail.com>
2023-06-27 23:25:04 +0300

    Complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,17 +3,28 @@ class MoviesController < ApplicationController
 
   def index
     movies = Movie.all
-    render json: movies
+    render json: movies 
   end
 
   def show
     movie = Movie.find(params[:id])
-    render json: movie
+    render json: movie 
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+  
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
 end
+
